5748823def2f32307828f8946cfbbb2433e15102
noah <wigginno@oregonstate.edu>
2021-12-03 10:19:46 -0500

    Completed delete and update functionality

--- app/webapp.py
+++ app/webapp.py
@@ -3,6 +3,13 @@ from flask import request, redirect
 from db_connector import connect_to_database, execute_query
 webapp = Flask(__name__)
 
+webapp.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# TODO - fix issue on update player:
+#   Cannot delete or update a parent row: a foreign key constraint fails
+#       (`chesstables`.`games`, CONSTRAINT `fk_black_player` FOREIGN KEY
+#       (`black_player`) REFERENCES `players` (`player_id`))')
+
 @webapp.route('/')
 def index():
     return render_template('index.html')
@@ -14,7 +21,7 @@ def Games():
          WHERE table_schema = \"chesstables\" and table_name = \"games\";"
     
     next_game_id = execute_query(db_connection, next_game_id_query).fetchall()
-    last_game_id = next_game_id[0][0] - 1
+    last_game_id = next_game_id[0]["AUTO_INCREMENT"] - 1
     
     last_page = last_game_id // 500 + 1
     page_number = int(request.args.get('page', 1))
@@ -25,16 +32,7 @@ def Games():
     get_games_query = "SELECT * FROM games WHERE game_id>=%d AND game_id<=%d;"\
          % (min_id, max_id)
     games = execute_query(db_connection, get_games_query).fetchall()
-    
-    get_openings_query = "SELECT opening_id FROM openings;"
-    openings = execute_query(db_connection, get_openings_query).fetchall()
-    openings_list = []
-    
-    for o in openings:
-        id=str(o)[2:5]
-        openings_list.append(id)
-    
-    openings = openings_list
+    openings = get_openings(db_connection)
 
     return render_template('Games.html', openings=openings, games=games,\
          page_number=page_number, last_page=last_page)
@@ -42,7 +40,7 @@ def Games():
 @webapp.route('/openings')
 def Openings():
     db_connection = connect_to_database()
-    get_openings_query = "SELECT * FROM openings;"
+    get_openings_query = "SELECT opening_id, opening_name FROM openings;"
     openings = execute_query(db_connection, get_openings_query).fetchall()
     
     return render_template('Openings.html', openings=openings)
@@ -52,16 +50,7 @@ def Players():
     db_connection = connect_to_database()
     get_players_query = "SELECT * FROM players;"
     players = execute_query(db_connection, get_players_query).fetchall()
-
-    get_openings_query = "SELECT opening_id FROM openings;"
-    openings = execute_query(db_connection, get_openings_query).fetchall()
-    openings_list = []
-    
-    for o in openings:
-        id=str(o)[2:5]
-        openings_list.append(id)
-    
-    openings = openings_list
+    openings = get_openings(db_connection)
 
     return render_template('Players.html', players=players, openings=openings)
 
@@ -78,7 +67,10 @@ def addrating():
     rating_name = request.form['rating_name']
     add_rating_query = "INSERT INTO ratings (rating_name) VALUES (%s);"\
          % (rating_name)
-    execute_query(db_connection, add_rating_query)
+    try:
+        execute_query(db_connection, add_rating_query)
+    except:
+        pass
     return redirect('/ratings')
 
 @webapp.route('/addopening', methods=['POST'])
@@ -88,7 +80,10 @@ def addopening():
     opening_name=request.form['opening_name']
     add_opening_query = '''INSERT INTO openings (opening_id, opening_name) 
         VALUES (\'%s\', \'%s\');''' % (opening_id, opening_name)
-    execute_query(db_connection, add_opening_query)
+    try:
+        execute_query(db_connection, add_opening_query)
+    except:
+        pass
     return redirect('/openings')
 
 @webapp.route('/addplayer', methods=['POST'])
@@ -100,7 +95,12 @@ def addplayer():
     add_opening_query = '''INSERT INTO players (player_id, rating_name, 
         favorite_opening) VALUES (\'%s\', \'%s\', \'%s\');''' %\
              (player_id, rating_name, favorite_opening)
-    execute_query(db_connection, add_opening_query)
+    
+    try:
+        execute_query(db_connection, add_opening_query)
+    except:
+        pass
+
     return redirect('/players')
 
 @webapp.route('/addgame', methods=['POST'])
@@ -113,7 +113,12 @@ def addgame():
     add_game_query = '''INSERT INTO games (white_player, black_player, result, 
         opening_id) VALUES (\'%s\', \'%s\', \'%s\', \'%s\')''' %\
          (white_player, black_player, result, opening_id)
-    execute_query(db_connection, add_game_query)
+    
+    try:
+        execute_query(db_connection, add_game_query)
+    except:
+        pass
+
     return redirect('/games')
 
 @webapp.route('/filtergames')
@@ -165,106 +170,233 @@ def filterplayers():
 
     return render_template('filterplayers.html', players=players)
 
-@webapp.route('/deleterating', methods=['POST'])
+@webapp.route('/deleterating')
 def deleterating():
     rating_name = request.args.get('rating_name')
     
     if rating_name:
         db_connection = connect_to_database()
-        del_rating_query = ""
-        execute_query(db_connection, del_rating_query)
+        del_rating_query = "DELETE FROM ratings WHERE rating_name='%s';"\
+             % rating_name
+        try:
+            execute_query(db_connection, del_rating_query)
+        except:
+            pass
 
     return redirect('/ratings')
 
-@webapp.route('/deleteopening', methods=['POST'])
+@webapp.route('/deleteopening')
 def deleteopening():
     opening_id = request.args.get('opening_id')
     
     if opening_id:
         db_connection = connect_to_database()
-        del_opening_query = ""
-        execute_query(db_connection, del_opening_query)
+        del_opening_query = "DELETE FROM openings WHERE opening_id='%s';"\
+             % opening_id
+        try:
+            execute_query(db_connection, del_opening_query)
+        except:
+            pass
 
     return redirect('/openings')
     
 
-@webapp.route('/deleteplayer', methods=['POST'])
+@webapp.route('/deleteplayer')
 def deleteplayer():
     player_id = request.args.get('player_id')
 
     if player_id:
         db_connection = connect_to_database()
-        del_player_query = ""
-        execute_query(db_connection, del_player_query)
+        del_player_query = "DELETE FROM players WHERE player_id='%s';"\
+             % player_id
+        try:
+            execute_query(db_connection, del_player_query)
+        except:
+            pass
 
     return redirect('/players')
 
-@webapp.route('/deletegame', methods=['POST'])
+@webapp.route('/deletegame')
 def deletegame():
     game_id = request.args.get('game_id')
 
     if game_id:
         db_connection = connect_to_database()
-        del_game_query = ""
-        execute_query(db_connection, del_game_query)
+        del_game_query = "DELETE FROM games WHERE game_id=%s;" % (game_id)
+        try:
+            execute_query(db_connection, del_game_query)
+        except:
+            pass
 
     return redirect('/games')
 
 @webapp.route('/updaterating', methods=['POST'])
 def updaterating():
-    rating_name = request.args.get('rating_name')
+    rating_name = request.form.get('id')
+    new_rating_name = request.form.get('rating_name')
     
-    if rating_name:
+    if new_rating_name:
         db_connection = connect_to_database()
-        update_rating_name_query = ""
-        execute_query(db_connection, update_rating_name_query)
+        update_rating_name_query = "UPDATE ratings SET rating_name='%s'\
+         WHERE rating_name='%s';" % rating_name
+        try:
+            execute_query(db_connection, update_rating_name_query)
+        except:
+            pass
 
     return redirect('/ratings')
 
 @webapp.route('/updateopening', methods=['POST'])
 def updateopening():
-    opening_id = request.args.get('opening_id')
-    opening_name = request.args.get('opening_name')
+    opening_id = request.form.get('id')
+    new_opening_id = request.form.get('opening_id')
+    opening_name = request.form.get('opening_name')
 
-    if opening_id:
+    if new_opening_id:
         db_connection = connect_to_database()
-        update_opening_id_query = ""
-        execute_query(db_connection, update_opening_id_query)
+        update_opening_id_query = "UPDATE openings SET opening_id='%s'\
+         WHERE opening_id='%s';" % (new_opening_id, opening_id)
+        try:
+            execute_query(db_connection, update_opening_id_query)
+        except:
+            pass
 
     if opening_name:
         db_connection = connect_to_database()
-        update_opening_name_query = ""
-        execute_query(db_connection, update_opening_name_query)
-    
+        update_opening_name_query = "UPDATE openings SET opening_name='%s'\
+         WHERE opening_id='%s';" % (opening_name, opening_id)
+        try:
+            execute_query(db_connection, update_opening_name_query)
+        except:
+            pass
+
     return redirect('/openings')
 
 @webapp.route('/updateplayer', methods=['POST'])
 def updateplayer():
-    player_id = request.args.get('player_id')
-    rating_name = request.args.get('rating_name')
-    favorite_opening = request.args.get('favorite_opening')
+    player_id = request.form['id']
+    new_player_id = request.form.get('player_id')
+    rating_name = request.form.get('rating_name')
+    favorite_opening = request.form.get('favorite_opening')
 
     if player_id:
         db_connection = connect_to_database()
-        update_player_id_query = ""
-        execute_query(db_connection, update_player_id_query)
+        update_player_id_query = "UPDATE players SET player_id='%s'\
+             WHERE player_id='%s';" % (new_player_id, player_id)
+        try:
+            execute_query(db_connection, update_player_id_query)
+        except:
+            pass
+
     if rating_name:
         db_connection = connect_to_database()
-        update_rating_name_query = "UPDATE openings SET rating_name=%s WHERE rating_name="
-        execute_query(db_connection, update_rating_name_query)
+        update_rating_name_query = "UPDATE players SET rating_name='%s'\
+             WHERE player_id='%s';" % (rating_name, player_id)
+        try:
+            execute_query(db_connection, update_rating_name_query)
+        except:
+            pass
+
     if favorite_opening:
         db_connection = connect_to_database()
-        update_favorite_opening_query = ""
-        execute_query(db_connection, update_favorite_opening_query)
+        update_fav_opening_query = "UPDATE players SET favorite_opening='%s'\
+             WHERE player_id='%s';" % (favorite_opening, player_id)
+        try:
+            execute_query(db_connection, update_fav_opening_query)
+        except:
+            pass
 
     return redirect('/players')
 
 @webapp.route('/updategame', methods=['POST'])
 def updategame():
-    game_id = request.args.get('game_id')
-    black_player = request.args.get('black_player')
-    white_player = request.args.get('white_player')
-    result = request.args.get('result')
-    opening_id = request.args.get('opening_id')
+    game_id = request.form.get('game_id')
+    white_player = request.form.get('white_player')
+    black_player = request.form.get('black_player')
+    result = request.form.get('result')
+    opening_id = request.form.get('opening_id')
+    
+    if white_player:
+        db_connection = connect_to_database()
+        update_white_player_id_query = "UPDATE games SET white_player='%s'\
+             WHERE game_id='%s';" % (white_player, game_id)
+        try:
+            execute_query(db_connection, update_white_player_id_query)
+        except:
+            pass
+
+    if black_player:
+        db_connection = connect_to_database()
+        update_black_player_id_query = "UPDATE games SET black_player='%s'\
+             WHERE game_id='%s';" % (black_player, game_id)
+        try:
+            execute_query(db_connection, update_black_player_id_query)
+        except:
+            pass
+
+    if result:
+        db_connection = connect_to_database()
+        update_result_query = "UPDATE games SET result='%s'\
+             WHERE game_id='%s';" % (result, game_id)
+        try:
+            execute_query(db_connection, update_result_query)
+        except:
+            pass
+
+    if opening_id:
+        db_connection = connect_to_database()
+        update_opening_id_query = "UPDATE games SET opening_id='%s'\
+             WHERE game_id='%s';" % (opening_id, game_id)
+        try:
+            execute_query(db_connection, update_opening_id_query)
+        except:
+            pass
 
     return redirect('/games')
+
+@webapp.route('/updateplayerform')
+def updateplayerform():
+    db_connection = connect_to_database()
+    id = request.args.get('player_id')
+    attributes = ['player_id', 'rating_name', 'favorite_opening']
+    entity_name = 'player'
+    openings = get_openings(db_connection)
+    return render_template('updateform.html', id=id, attributes=attributes,\
+         entity_name=entity_name, openings=openings)
+
+@webapp.route('/updategameform')
+def updategameform():
+    db_connection = connect_to_database()
+    id = request.args.get('game_id')
+    attributes = ['game_id', 'white_player', 'black_player',\
+         'result', 'opening_id']
+    entity_name = 'game'
+    openings = get_openings(db_connection)
+    return render_template('updateform.html', id=id, attributes=attributes,\
+         entity_name=entity_name, openings=openings)
+
+@webapp.route('/updateopeningform')
+def updateopeningform():
+    id = request.args.get('opening_id')
+    attributes = ['opening_id', 'opening_name']
+    entity_name = 'opening'
+    return render_template('updateform.html', id=id, attributes=attributes,\
+         entity_name=entity_name)
+
+@webapp.route('/updateratingform')
+def updateratingform():
+    id = request.args.get('rating_name')
+    attributes = ['rating_name']
+    entity_name = 'rating'
+    return render_template('updateform.html', id=id, attributes=attributes,\
+         entity_name=entity_name)
+    
+def get_openings(db_connection):
+    get_openings_query = "SELECT opening_id FROM openings;"
+    openings = execute_query(db_connection, get_openings_query).fetchall()
+    return openings
+
+# update form variables:
+#   entity_name
+#   id
+#   attributes
--- queries/1_setup.sql
+++ queries/1_setup.sql
@@ -1,49 +1,47 @@
-
-DROP TABLE IF EXISTS ratings;
-CREATE TABLE ratings (
-  rating_name varchar(22) NOT NULL,
-  PRIMARY KEY (rating_name)
-) ENGINE = InnoDB; 
-
-
-DROP TABLE IF EXISTS openings;
-CREATE TABLE openings (
-  opening_id varchar(11) NOT NULL,
-  opening_name varchar(255) NOT NULL,
-  PRIMARY KEY (opening_id)
-) ENGINE = InnoDB;
-
-
+DROP TABLE IF EXISTS games;
 DROP TABLE IF EXISTS players;
+DROP TABLE IF EXISTS openings;
+DROP TABLE IF EXISTS ratings;
+
+CREATE TABLE ratings (
+  rating_name varchar(22) NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE openings (
+  opening_id varchar(11) NOT NULL PRIMARY KEY,
+  opening_name varchar(255) NOT NULL
+);
+
 CREATE TABLE players (
-  player_id varchar(17) NOT NULL,
+  player_id varchar(17) NOT NULL PRIMARY KEY,
   rating_name varchar(22)  NOT NULL,
   favorite_opening varchar(255)  NULL,
-  PRIMARY KEY (player_id),
   CONSTRAINT fk_rating_name
     FOREIGN KEY(rating_name)
-    REFERENCES ratings (rating_name),
+    REFERENCES ratings (rating_name)
+    ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT fk_favorite_opening
     FOREIGN KEY(favorite_opening)
     REFERENCES openings (opening_id)
-) ENGINE = InnoDB;
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-
-DROP TABLE IF EXISTS games;
 CREATE TABLE games (
-  game_id int(11) NOT NULL AUTO_INCREMENT,
+  game_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   white_player varchar(255)  NULL,
   black_player varchar(255)  NULL,
   result varchar(11)  NOT NULL,
   opening_id varchar(11)  NOT NULL,
-  PRIMARY KEY (game_id),
   CONSTRAINT fk_white_player
     FOREIGN KEY(white_player)
-    REFERENCES players (player_id),
+    REFERENCES players (player_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT fk_black_player
     FOREIGN KEY(black_player)
-    REFERENCES players (player_id),
+    REFERENCES players (player_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT fk_opening_id
     FOREIGN KEY(opening_id)
     REFERENCES openings (opening_id)
-) ENGINE = InnoDB;
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
